715abdc6e9ed3de5a6d5cda758e8d5e315a8af33
mubinsy651 <mubincdm@gmail.com>
2026-09-23 11:40:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -392,7 +392,7 @@ EOF
 
 cat>files/usr/share/Lenyu-version.sh<<-\EOF
 #!/bin/bash
-# https://github.com/mbsy3211/Actions-OpenWrt-x86
+# https://github.com/mubinsy651/ActionsOpenWrtx86
 # Actions-OpenWrt-x86 By Lenyu 20210505
 #path=$(dirname $(readlink -f $0))
 # cd ${path}
@@ -406,7 +406,7 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/mbsy3211/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+wget -qO- -t1 -T2 "https://api.github.com/repos/mubinsy651/ActionsOpenWrtx86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
 if [ -s  "/tmp/cloud_ts_version" ]; then
 cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
 fi
